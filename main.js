7c6db8168c046e9514ac78fceec9a1e0781b7c2e
Stanislao <stanislaoweb@libero.it>
2020-11-06 17:29:03 +0100

    Finito

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
+var listaImmagini = ['hello.jpeg', 'royal.jpg', 'pier.jpg'];
+var indiceImmagine = 0;
+
 var app = new Vue({
 
   el: '#root',
 
   data: {
-
     hello: 'Hello World',
     colorTitle: 'green',
-    firstPic: 'hello.jpeg',
-    secondPic: 'royal.jpg',
-    thirdPic: 'pier.jpg',
-    imgDisplayed: 'active',
-    imgNone: 'none'
+    imgSrc: listaImmagini[0],
   },
   methods: {
 
     changePics: function() {
 
-        if (this.firstPic == 'hello.jpeg') {
-          this.firstPic = this.secondPic;
+
+        indiceImmagine++;
+
+        if (indiceImmagine == listaImmagini.length) {
+
+          indiceImmagine = 0;
         }
 
-        if (this.firstPic == 'royal.jpg') {
-          this.firstPic = this.thirdPic;
-        };
+        console.log(listaImmagini[indiceImmagine]);
+
+        this.imgSrc = listaImmagini[indiceImmagine];
 
     },
 
